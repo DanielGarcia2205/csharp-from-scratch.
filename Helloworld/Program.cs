ebab6207859c452dc263061a3dbc8f585d282b5e
dgarcia <danigarcia222005@gmail.com>
2024-09-25 00:22:18 -0500

    change hello world class

--- Helloworld/Program.cs
+++ Helloworld/Program.cs
@@ -1,3 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
+﻿namespace Helloworld
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            var Name = "Jhon";
+            Name = "1Jhon";
+            Console.WriteLine($"Hello, world: {Name}");
+            Console.WriteLine();
+        }
+    }
+}
